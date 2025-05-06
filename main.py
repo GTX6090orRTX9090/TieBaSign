5774020ecb079400a406517b3df381b5896c481c
GTX6090orRTX9090 <76603354+GTX6090orRTX9090@users.noreply.github.com>
2025-05-06 22:18:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -225,7 +225,7 @@ def main():
             client_sign(i, tbs, j["id"], j["name"])
         logger.info("完成第" + str(n) + "个用户签到")
    # send_email(favorites)
-    logger.info("所有用户签到结束")
+    logger.info("所有用户签到结束。")
 
 
 if __name__ == '__main__':
